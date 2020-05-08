bac18f680fc530e99e6ec9972fea10d58742d26c
seokhui11111 <nsh1346@naver.com>
2020-05-08 10:26:52 +0900

    login 3

--- src/main/java/com/spring/controller/LoginController.java
+++ src/main/java/com/spring/controller/LoginController.java
@@ -1,14 +1,25 @@
 package com.spring.controller;
 
 
+import java.util.HashMap;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.spring.service.KakaoService;
+
 @Controller
 public class LoginController {
+	
+	@Autowired
+	private KakaoService kakao;
+	
 	
 	@GetMapping("/login")
 	public void login() {
@@ -42,5 +53,21 @@ public class LoginController {
 	@PostMapping("/register/step3")
 	public void step3() {
 		
+	}
+	
+	@GetMapping("/register/kakaologin")
+	public void kakaologin(@RequestParam("code") String code,HttpSession session) {
+        String access_Token = kakao.getAccessToken(code);
+        System.out.println("controller access_token : " + access_Token);
+		
+        HashMap<String, Object> userInfo = kakao.getUserInfo(access_Token);
+        System.out.println("login Controller : " + userInfo);
+        String nickname = (String) userInfo.get("nickname");
+        if (userInfo.get("email") != null) {
+            session.setAttribute("userId", userInfo.get("email"));
+            session.setAttribute("access_Token", access_Token);
+        }
+        session.setAttribute("nickname", nickname);
+			
 	}
 }
