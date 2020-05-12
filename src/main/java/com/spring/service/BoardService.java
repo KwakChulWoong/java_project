46198f873e0b17997bf614927284c095991dea71
dreamer9107 <dreaming91@naver.com>
2020-05-12 11:01:47 +0900

    1

--- src/main/java/com/spring/service/BoardService.java
+++ src/main/java/com/spring/service/BoardService.java
@@ -2,17 +2,17 @@ package com.spring.service;
 
 import java.util.List;
 
-import com.spring.domain.AttachFileDTO;
+
 import com.spring.domain.BoardVO;
-import com.spring.domain.Criteria;
+
 
 public interface BoardService {
 //	//게시글 작성
-	public boolean ItemRegister(BoardVO vo) throws Exception;
+//	public boolean ItemRegister(BoardVO vo) throws Exception;
 	//게시글 목록
 	public List<BoardVO> getList() throws Exception;
 //	//read 
-	public BoardVO readList(int bno) throws Exception;
+//	public BoardVO readList(int bno) throws Exception;
 //	//modify
 //	public boolean updateRow(BoardVO vo) throws Exception;
 //	//remove
